80d4363b74a56e03ae496ee1f00e9bba7af23aae
Georgijs Gordejevs <2georgeek@gmail.com>
2021-01-28 00:33:12 +0200

    Helper functions to translate numeric GTPv1 and GTPv2 msg types to string

---
+++ gtpv1/message/message_test.go
@@ -0,0 +1,34 @@
+package message
+
+import "testing"
+
+func TestMsgTypeStr(t *testing.T) {
+	tests := []struct {
+		name string
+		msgType uint8
+		want string
+	} {
+		{
+			name: "EchoRequest",
+			msgType: MsgTypeEchoRequest,
+			want: "EchoRequest",
+		},
+		{
+			name: "DataRecordTransferResponse",
+			msgType: MsgTypeDataRecordTransferResponse,
+			want: "DataRecordTransferResponse",
+		},
+		{
+			name: "Unknown msg type",
+			msgType: 14,
+			want: "14",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MsgTypeStr(tt.msgType); got != tt.want {
+				t.Errorf("MsgTypeStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
